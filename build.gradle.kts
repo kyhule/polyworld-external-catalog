76ed64b57c421173878541252cbe1943cd70492b
Kyle Lehman <kyle.h.lehman@gmail.com>
2023-06-01 07:23:04 -0400

    Sets up publishing to github packages via gradle-maven-publish-plugin
    https://github.com/vanniktech/gradle-maven-publish-plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,23 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     id("version-catalog")
+    alias(libs.plugins.mavenPublish)
     alias(libs.plugins.reckon)
 }
 
 catalog {
     versionCatalog {
         from(files("externalLibs.versions.toml"))
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "githubPackages"
+            url = uri("https://maven.pkg.github.com/kyhule/polyworld-external-catalog")
+            credentials(PasswordCredentials::class)
+        }
     }
 }
 
