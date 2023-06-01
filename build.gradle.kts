f5eadec6049af3a3985063d540b0b0c4f6fe5128
Kyle Lehman <kyle.h.lehman@gmail.com>
2023-06-01 07:15:12 -0400

    Sets up versioning via reckon
    https://github.com/ajoberstar/reckon

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,17 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     id("version-catalog")
+    alias(libs.plugins.reckon)
 }
 
 catalog {
     versionCatalog {
         from(files("externalLibs.versions.toml"))
     }
+}
+
+reckon {
+    setScopeCalc(calcScopeFromProp())
+    snapshots()
+    setStageCalc(calcStageFromProp())
 }
